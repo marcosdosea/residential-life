aa3260a0c97e1f37688350d3c0c9e6a0bd0afe5d
thiago7a@gmail.com <thiago7a@gmail.com@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-06-17 13:13:29 +0000

    Inserção dos artefatos gerados até o momento em ESI.

---
+++ trunk/AnaliseProjeto/Protótipos/Copy_of_CSU03___Gerenciar_Central_de_atendimento_files/axurerp_pagespecificscript.js
@@ -0,0 +1,602 @@
+﻿for(var i = 0; i < 170; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u115'] = 'top';
+u122.style.cursor = 'pointer';
+$axure.eventManager.click('u122', function(e) {
+
+if (true) {
+
+	var obj1 = document.getElementById("u121");
+    obj1.disabled = false;
+
+	var obj1 = document.getElementById("u122");
+    obj1.disabled = true;
+
+}
+});
+
+u21.style.cursor = 'pointer';
+$axure.eventManager.click('u21', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd7u11','none','',500,'none','',500);
+
+}
+});
+
+u53.style.cursor = 'pointer';
+$axure.eventManager.click('u53', function(e) {
+
+if (true) {
+
+	var obj1 = document.getElementById("u53");
+    obj1.disabled = true;
+
+}
+});
+document.getElementById('u7_img').tabIndex = 0;
+
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd2u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u79'] = 'top';document.getElementById('u110_img').tabIndex = 0;
+
+u110.style.cursor = 'pointer';
+$axure.eventManager.click('u110', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd3u11','none','',500,'none','',500);
+
+}
+});
+document.getElementById('u140_img').tabIndex = 0;
+
+u140.style.cursor = 'pointer';
+$axure.eventManager.click('u140', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd1u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u135'] = 'top';gv_vAlignTable['u151'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u103'] = 'top';u159.tabIndex = 0;
+
+u159.style.cursor = 'pointer';
+$axure.eventManager.click('u159', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Copy_of_CSU23___Visualizar_movimenta_ao_financeira.html');
+
+}
+});
+gv_vAlignTable['u159'] = 'top';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u101'] = 'top';document.getElementById('u5_img').tabIndex = 0;
+
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd0u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u105'] = 'top';gv_vAlignTable['u27'] = 'top';
+u52.style.cursor = 'pointer';
+$axure.eventManager.click('u52', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd6u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u67'] = 'top';
+u120.style.cursor = 'pointer';
+$axure.eventManager.click('u120', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd6u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u48'] = 'top';
+u37.style.cursor = 'pointer';
+$axure.eventManager.click('u37', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd7u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u75'] = 'top';gv_vAlignTable['u133'] = 'top';document.getElementById('u68_img').tabIndex = 0;
+
+u68.style.cursor = 'pointer';
+$axure.eventManager.click('u68', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd5u11','none','',500,'none','',500);
+
+}
+});
+u161.tabIndex = 0;
+
+u161.style.cursor = 'pointer';
+$axure.eventManager.click('u161', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU19___Gerenciar_enquetes.html');
+
+}
+});
+gv_vAlignTable['u161'] = 'top';document.getElementById('u72_img').tabIndex = 0;
+
+u72.style.cursor = 'pointer';
+$axure.eventManager.click('u72', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd5u11','none','',500,'none','',500);
+
+}
+});
+u164.tabIndex = 0;
+
+u164.style.cursor = 'pointer';
+$axure.eventManager.click('u164', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU07___Gerenciar_Funcionarios.html');
+
+}
+});
+gv_vAlignTable['u164'] = 'top';gv_vAlignTable['u99'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u149'] = 'top';
+u54.style.cursor = 'pointer';
+$axure.eventManager.click('u54', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd4u11','none','',500,'none','',500);
+
+}
+});
+
+u88.style.cursor = 'pointer';
+$axure.eventManager.click('u88', function(e) {
+
+if (true) {
+
+	var obj1 = document.getElementById("u88");
+    obj1.disabled = true;
+
+}
+});
+
+u38.style.cursor = 'pointer';
+$axure.eventManager.click('u38', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd1u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u119'] = 'top';gv_vAlignTable['u85'] = 'top';
+u51.style.cursor = 'pointer';
+$axure.eventManager.click('u51', function(e) {
+
+if (true) {
+
+	SetPanelState('u47', 'pd2u47','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u77'] = 'top';gv_vAlignTable['u153'] = 'top';gv_vAlignTable['u31'] = 'top';u166.tabIndex = 0;
+
+u166.style.cursor = 'pointer';
+$axure.eventManager.click('u166', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Copy_of_CSU02___Gerenciar_espa_os.html');
+
+}
+});
+gv_vAlignTable['u166'] = 'top';
+u36.style.cursor = 'pointer';
+$axure.eventManager.click('u36', function(e) {
+
+if (true) {
+
+	SetPanelState('u39', 'pd0u39','none','',500,'none','',500);
+
+}
+});
+u158.tabIndex = 0;
+
+u158.style.cursor = 'pointer';
+$axure.eventManager.click('u158', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU12___Publicar_no_mural_eletronico__Sindico_.html');
+
+}
+});
+gv_vAlignTable['u158'] = 'top';
+u123.style.cursor = 'pointer';
+$axure.eventManager.click('u123', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd0u11','none','',500,'none','',500);
+
+}
+});
+document.getElementById('u114_img').tabIndex = 0;
+
+u114.style.cursor = 'pointer';
+$axure.eventManager.click('u114', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd3u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u33'] = 'top';u160.tabIndex = 0;
+
+u160.style.cursor = 'pointer';
+$axure.eventManager.click('u160', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU17___Gerenciar_Reservas_de_Ambientes.html');
+
+}
+});
+gv_vAlignTable['u160'] = 'top';gv_vAlignTable['u157'] = 'center';
+u92.style.cursor = 'pointer';
+$axure.eventManager.click('u92', function(e) {
+
+if (true) {
+
+}
+});
+
+u46.style.cursor = 'pointer';
+$axure.eventManager.click('u46', u46Click);
+InsertAfterBegin(document.body, "<div class='intcases' id='u46LinksClick'></div>")
+var u46LinksClick = document.getElementById('u46LinksClick');
+function u46Click(e) 
+{
+windowEvent = e;
+
+
+	ToggleLinks(e, 'u46LinksClick');
+}
+
+InsertBeforeEnd(u46LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u46Clicku75253f1b79634d35b141b4b6190c7c77(event)'>caixa de entrada</div>");
+function u46Clicku75253f1b79634d35b141b4b6190c7c77(e)
+{
+
+	SetPanelState('u11', 'pd0u11','none','',500,'none','',500);
+
+	ToggleLinks(e, 'u46LinksClick');
+}
+
+InsertBeforeEnd(u46LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u46Clicku61e644dd78ab46c9994e465dec96ee25(event)'>resolvidos</div>");
+function u46Clicku61e644dd78ab46c9994e465dec96ee25(e)
+{
+
+	SetPanelState('u11', 'pd2u11','none','',500,'none','',500);
+
+	ToggleLinks(e, 'u46LinksClick');
+}
+
+InsertBeforeEnd(u46LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u46Clicku07d9f616487a4513a2105056666590c4(event)'>em análise</div>");
+function u46Clicku07d9f616487a4513a2105056666590c4(e)
+{
+
+	SetPanelState('u11', 'pd4u11','none','',500,'none','',500);
+
+	ToggleLinks(e, 'u46LinksClick');
+}
+gv_vAlignTable['u137'] = 'top';gv_vAlignTable['u71'] = 'top';document.getElementById('u136_img').tabIndex = 0;
+
+u136.style.cursor = 'pointer';
+$axure.eventManager.click('u136', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd1u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u169'] = 'center';
+u42.style.cursor = 'pointer';
+$axure.eventManager.click('u42', u42Click);
+InsertAfterBegin(document.body, "<div class='intcases' id='u42LinksClick'></div>")
+var u42LinksClick = document.getElementById('u42LinksClick');
+function u42Click(e) 
+{
+windowEvent = e;
+
+
+	ToggleLinks(e, 'u42LinksClick');
+}
+
+InsertBeforeEnd(u42LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u42Clickubfbaa3c3368b435db56d9d1673c8bdbc(event)'>erro ao enviar</div>");
+function u42Clickubfbaa3c3368b435db56d9d1673c8bdbc(e)
+{
+
+	ToggleLinks(e, 'u42LinksClick');
+}
+
+InsertBeforeEnd(u42LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u42Clicku85858686032e43818ec5aa49484fdd6e(event)'>enviado</div>");
+function u42Clicku85858686032e43818ec5aa49484fdd6e(e)
+{
+
+	ToggleLinks(e, 'u42LinksClick');
+}
+document.getElementById('u106_img').tabIndex = 0;
+
+u106.style.cursor = 'pointer';
+$axure.eventManager.click('u106', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd3u11','none','',500,'none','',500);
+
+}
+});
+document.getElementById('u154_img').tabIndex = 0;
+
+u154.style.cursor = 'pointer';
+$axure.eventManager.click('u154', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU22___Autentica_ao.html');
+
+}
+});
+gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u139'] = 'top';
+u87.style.cursor = 'pointer';
+$axure.eventManager.click('u87', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd6u11','none','',500,'none','',500);
+
+}
+});
+
+u121.style.cursor = 'pointer';
+$axure.eventManager.click('u121', function(e) {
+
+if (true) {
+
+	var obj1 = document.getElementById("u121");
+    obj1.disabled = true;
+
+	var obj1 = document.getElementById("u122");
+    obj1.disabled = false;
+
+}
+});
+document.getElementById('u102_img').tabIndex = 0;
+
+u102.style.cursor = 'pointer';
+$axure.eventManager.click('u102', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd3u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u155'] = 'center';gv_vAlignTable['u109'] = 'top';gv_vAlignTable['u50'] = 'top';gv_vAlignTable['u97'] = 'top';gv_vAlignTable['u63'] = 'top';document.getElementById('u76_img').tabIndex = 0;
+
+u76.style.cursor = 'pointer';
+$axure.eventManager.click('u76', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd5u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u81'] = 'top';
+u128.style.cursor = 'pointer';
+$axure.eventManager.click('u128', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Menu_Sindico.html');
+
+}
+});
+
+u94.style.cursor = 'pointer';
+$axure.eventManager.click('u94', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Menu_Sindico.html');
+
+}
+});
+
+u60.style.cursor = 'pointer';
+$axure.eventManager.click('u60', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Menu_Sindico.html');
+
+}
+});
+document.getElementById('u9_img').tabIndex = 0;
+
+u9.style.cursor = 'pointer';
+$axure.eventManager.click('u9', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd4u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u73'] = 'top';gv_vAlignTable['u113'] = 'top';gv_vAlignTable['u147'] = 'top';u163.tabIndex = 0;
+
+u163.style.cursor = 'pointer';
+$axure.eventManager.click('u163', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU20___Gerenciar_emails.html');
+
+}
+});
+gv_vAlignTable['u163'] = 'top';gv_vAlignTable['u91'] = 'top';gv_vAlignTable['u131'] = 'top';gv_vAlignTable['u18'] = 'top';u162.tabIndex = 0;
+
+u162.style.cursor = 'pointer';
+$axure.eventManager.click('u162', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU16___Gerenciar_ocorrencias.html');
+
+}
+});
+gv_vAlignTable['u162'] = 'top';
+u126.style.cursor = 'pointer';
+$axure.eventManager.click('u126', function(e) {
+
+if (true) {
+
+}
+});
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', u13Click);
+InsertAfterBegin(document.body, "<div class='intcases' id='u13LinksClick'></div>")
+var u13LinksClick = document.getElementById('u13LinksClick');
+function u13Click(e) 
+{
+windowEvent = e;
+
+
+	ToggleLinks(e, 'u13LinksClick');
+}
+
+InsertBeforeEnd(u13LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u13Clicku8e4c446ad6194d32884b614d73ea4f91(event)'>erro ao enviar</div>");
+function u13Clicku8e4c446ad6194d32884b614d73ea4f91(e)
+{
+
+	SetPanelState('u17', 'pd1u17','none','',500,'none','',500);
+
+	ToggleLinks(e, 'u13LinksClick');
+}
+
+InsertBeforeEnd(u13LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u13Clicku458b1fdaf0e54a1c8d0494faf082fd90(event)'>enviado</div>");
+function u13Clicku458b1fdaf0e54a1c8d0494faf082fd90(e)
+{
+
+	SetPanelState('u17', 'pd0u17','none','',500,'none','',500);
+
+	ToggleLinks(e, 'u13LinksClick');
+}
+gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u141'] = 'top';gv_vAlignTable['u143'] = 'top';
+u58.style.cursor = 'pointer';
+$axure.eventManager.click('u58', function(e) {
+
+if (true) {
+
+}
+});
+gv_vAlignTable['u127'] = 'top';gv_vAlignTable['u111'] = 'top';gv_vAlignTable['u145'] = 'top';gv_vAlignTable['u83'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u124'] = 'top';document.getElementById('u144_img').tabIndex = 0;
+
+u144.style.cursor = 'pointer';
+$axure.eventManager.click('u144', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd1u11','none','',500,'none','',500);
+
+}
+});
+document.getElementById('u80_img').tabIndex = 0;
+
+u80.style.cursor = 'pointer';
+$axure.eventManager.click('u80', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd5u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u1'] = 'center';document.getElementById('u148_img').tabIndex = 0;
+
+u148.style.cursor = 'pointer';
+$axure.eventManager.click('u148', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd1u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u93'] = 'top';u167.tabIndex = 0;
+
+u167.style.cursor = 'pointer';
+$axure.eventManager.click('u167', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU06___Gerenciar_cadastro_do_proprietario.html');
+
+}
+});
+gv_vAlignTable['u167'] = 'top';u165.tabIndex = 0;
+
+u165.style.cursor = 'pointer';
+$axure.eventManager.click('u165', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u165'] = 'top';gv_vAlignTable['u117'] = 'top';gv_vAlignTable['u59'] = 'top';
+u90.style.cursor = 'pointer';
+$axure.eventManager.click('u90', function(e) {
+
+if (true) {
+
+	SetPanelState('u11', 'pd2u11','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u107'] = 'top';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u69'] = 'top';
