8c2a4c2927dac8b607600c5c71e01d44e72da3cf
thiago7a@gmail.com <thiago7a@gmail.com@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-07-16 01:10:20 +0000

    Tag 2.0 - Entrga ES2

---
+++ tags/2.0/trunk/Requisitos/Protótipos/Menu_Sindico_files/axurerp_pagespecificscript.js
@@ -0,0 +1,161 @@
+﻿for(var i = 0; i < 23; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+document.getElementById('u21_img').tabIndex = 0;
+
+u21.style.cursor = 'pointer';
+$axure.eventManager.click('u21', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU22___Autentica_ao.html');
+
+}
+});
+u16.tabIndex = 0;
+
+u16.style.cursor = 'pointer';
+$axure.eventManager.click('u16', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU06___Gerenciar_cadastro_do_proprietario.html');
+
+}
+});
+gv_vAlignTable['u16'] = 'top';u12.tabIndex = 0;
+
+u12.style.cursor = 'pointer';
+$axure.eventManager.click('u12', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU20___Gerenciar_emails.html');
+
+}
+});
+gv_vAlignTable['u12'] = 'top';u15.tabIndex = 0;
+
+u15.style.cursor = 'pointer';
+$axure.eventManager.click('u15', function(e) {
+
+if (true) {
+
+	self.location.href='#';
+
+}
+});
+gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u4'] = 'center';u8.tabIndex = 0;
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Copy_of_CSU23___Visualizar_movimenta_ao_financeira.html');
+
+}
+});
+gv_vAlignTable['u8'] = 'top';u19.tabIndex = 0;
+
+u19.style.cursor = 'pointer';
+$axure.eventManager.click('u19', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Menu_Morador.html');
+
+}
+});
+gv_vAlignTable['u19'] = 'top';u10.tabIndex = 0;
+
+u10.style.cursor = 'pointer';
+$axure.eventManager.click('u10', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU19___Gerenciar_enquetes.html');
+
+}
+});
+gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u22'] = 'center';document.getElementById('u5_img').tabIndex = 0;
+
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU12___Publicar_no_mural_eletronico__Sindico_.html');
+
+}
+});
+gv_vAlignTable['u1'] = 'center';u9.tabIndex = 0;
+
+u9.style.cursor = 'pointer';
+$axure.eventManager.click('u9', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU17___Gerenciar_Reservas_de_Ambientes.html');
+
+}
+});
+gv_vAlignTable['u9'] = 'top';u14.tabIndex = 0;
+
+u14.style.cursor = 'pointer';
+$axure.eventManager.click('u14', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Copy_of_CSU03___Gerenciar_Central_de_atendimento.html');
+
+}
+});
+gv_vAlignTable['u14'] = 'top';u20.tabIndex = 0;
+
+u20.style.cursor = 'pointer';
+$axure.eventManager.click('u20', function(e) {
+
+if (true) {
+
+	SetPanelState('u2', 'pd1u2','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u6'] = 'center';u11.tabIndex = 0;
+
+u11.style.cursor = 'pointer';
+$axure.eventManager.click('u11', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU16___Gerenciar_ocorrencias.html');
+
+}
+});
+gv_vAlignTable['u11'] = 'top';u13.tabIndex = 0;
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU07___Gerenciar_Funcionarios.html');
+
+}
+});
+gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u18'] = 'center';u7.tabIndex = 0;
+
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('CSU12___Publicar_no_mural_eletronico__Sindico_.html');
+
+}
+});
+gv_vAlignTable['u7'] = 'top';
