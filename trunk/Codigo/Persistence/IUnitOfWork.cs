759489ff9759b07418f91222c57122a3ef186da7
thiago7a@gmail.com <thiago7a@gmail.com@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-08-12 23:18:09 +0000

    Criação do Projeto no Visual Studio.

---
+++ trunk/Codigo/Persistence/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Persistence;
+using Models;
+namespace Persistence
+{
+    public interface IUnitOfWork
+    {
+        void Commit(bool shared);
+        IRepositorioGenerico<tb_pessoa> RepositorioPessoa { get; }
+    
+    }
+}
