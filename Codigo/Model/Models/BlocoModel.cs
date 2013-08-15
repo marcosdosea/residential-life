f709eae94390da0e643507cfe744f3a173177e67
leonardosanso <leonardosanso@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-08-15 15:07:03 +0000

    Correções nomenclatura

--- Codigo/Model/Models/BlocoModel.cs
+++ Codigo/Model/Models/BlocoModel.cs
@@ -8,21 +8,21 @@ namespace Models.Models
 {
     public class BlocoModel
     {
-        public int idBloco { get; set; }
-        public int idCondominio { get; set; }
+        public int IDBloco { get; set; }
+        public int IDCondominio { get; set; }
        
         [Required]
         [Display(Name = "Nome")]
         [StringLength(100)]
-        public string nome { get; set; }
+        public string Nome { get; set; }
 
         [Required]
         [Display(Name = "Quantidade de Andares")]
-        public int quantAndares { get; set; }
+        public int QuantAndares { get; set; }
 
         [Required]
         [Display(Name = "Quantidade de Moradias")]
-        public int quantMoradias { get; set; }
+        public int QuantMoradias { get; set; }
 
 
     }
