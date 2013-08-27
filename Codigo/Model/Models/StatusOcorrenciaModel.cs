8275b04364704d20bd9f6f761303cdf502d72cba
nathanvasc <nathanvasc@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-08-27 01:05:36 +0000

    Ajustes ocorrencia

---
+++ Codigo/Model/Models/StatusOcorrenciaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models.Models
+{
+    public  class StatusOcorrenciaModel
+    {
+        public int IdStatusOcorrencia { get; set; }
+
+        public string StatusOcorrencia { get; set; }
+    }
+}
