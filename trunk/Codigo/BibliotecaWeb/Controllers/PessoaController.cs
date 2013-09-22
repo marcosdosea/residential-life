ee565572bf292a777bf578a15e0dead52ed89049
nathanvasc <nathanvasc@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-09-22 21:06:46 +0000

    Criação de relatórios Pessoa, postagem e reservas

--- trunk/Codigo/BibliotecaWeb/Controllers/PessoaController.cs
+++ trunk/Codigo/BibliotecaWeb/Controllers/PessoaController.cs
@@ -107,6 +107,51 @@ namespace BibliotecaWeb.Controllers
             base.Dispose(disposing);
         }
 
+        public ActionResult RelatorioPessoasPorEstado()
+        {
+            LocalReport relatorio = new LocalReport();
+
+            //Caminho onde o arquivo do Report Viewer está localizado
+            relatorio.ReportPath = Server.MapPath("~/Relatorios/RepostListaPessoasPorEstado.rdlc");
+            //Define o nome do nosso DataSource e qual rotina irá preenche-lo, no caso, nosso método criado anteriormente
+            relatorio.DataSources.Add(new ReportDataSource("DataSetPessoasPorEstado", gPessoa.ObterTodos()));
+
+            string reportType = "PDF";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+            string deviceInfo =
+             "<DeviceInfo>" +
+             " <OutputFormat>PDF</OutputFormat>" +
+             " <PageWidth>9in</PageWidth>" +
+             " <PageHeight>11in</PageHeight>" +
+             " <MarginTop>0.7in</MarginTop>" +
+             " <MarginLeft>2in</MarginLeft>" +
+             " <MarginRight>2in</MarginRight>" +
+             " <MarginBottom>0.7in</MarginBottom>" +
+             "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] bytes;
+
+            //Renderiza o relatório em bytes
+            bytes = relatorio.Render(
+            reportType,
+            deviceInfo,
+            out mimeType,
+            out encoding,
+            out fileNameExtension,
+            out streams,
+            out warnings);
+
+            return File(bytes, mimeType);
+
+        }
+    
+
+
         public ActionResult RelatorioPessoa()
         {
             LocalReport relatorio = new LocalReport();
