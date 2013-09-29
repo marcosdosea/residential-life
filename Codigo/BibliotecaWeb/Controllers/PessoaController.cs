df88d4ecf8c8cf462505c209f85eb6eb463280da
thiago7a <thiago7a@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2013-09-29 15:49:39 +0000

    Criação de usuário no membership no momento do cadastro de pessoa, e aribuição de papel default.

--- Codigo/BibliotecaWeb/Controllers/PessoaController.cs
+++ Codigo/BibliotecaWeb/Controllers/PessoaController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Models;
 using Services;
 using Microsoft.Reporting.WebForms;
+using System.Web.Security;
 
 namespace BibliotecaWeb.Controllers
 { 
@@ -52,6 +53,19 @@ namespace BibliotecaWeb.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Attempt to register the user
+                MembershipCreateStatus createStatus;
+                Membership.CreateUser(pessoaModel.Login, pessoaModel.Senha, pessoaModel.Email, "-", "-", true, out createStatus);
+
+                if (createStatus == MembershipCreateStatus.Success)
+                {
+                    FormsAuthentication.SetAuthCookie(pessoaModel.Login, false /* createPersistentCookie */);
+                }
+                else
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(createStatus));
+                }
+                Roles.AddUserToRole(pessoaModel.Login, "Morador");
                 gPessoa.Inserir(pessoaModel);
                 return RedirectToAction("Index");  
             }
@@ -194,5 +208,45 @@ namespace BibliotecaWeb.Controllers
 
         }
 
+
+        #region Status Codes
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "User name already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+        #endregion
     }
 }
