3a8cff7d1e0d0027b1fd029919fe1d1c8ea71b90
andres.s.menendez@gmail.com <andres.s.menendez@gmail.com@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2014-08-05 00:49:10 +0000

    Alteração do arquivo SQL que inicializa o Banco

--- Codigo/inicializaBD.sql
+++ Codigo/inicializaBD.sql
@@ -1,43 +1,5 @@
-INSERT INTO `tb_statusenquete` (`IdStatusEnquete`, `StatusEnquete`) VALUES
-(1, 'Ativa'),
-(2, 'Congelada'),
-(3, 'Finalizada');
-
-insert into tb_statusareapublica(IdStatusAreaPublica,StatusAreaPublica)values
-(1,'Disponível'),
-(2,'Indisponível');
-
-insert into tb_statuspagamento(IdStatusPagamento, StatusPagamento)
-values
-(1,"Pago"),
-(2,"Não Pago");
-
-
-insert into tb_statusocorrencia(IdStatusOcorrencia,StatusOcorrencia)
-values(1,'Em análise'),
-(2,'Resolvida');
-
-insert into tb_tipoocorrencia(IdTipoOcorrencia,TipoOcorrencia)
-values(1,'Barulho'),
-(2,'Vizinho'),
-(3,'Sujeira');
-
-insert into tb_tipoveiculo(IdTipoVeiculo,TipoVeiculo)
-values(1,'Moto'),
-(2,'Carro'),
-(3,'Caminhão');
-
-insert into tb_tipoplanodeconta(IdTipoPlanoDeConta, TipoPlanoDeConta)
-values(1, 'Despesa Ordinária'),
-(2, 'Despesa Extraordinária'),
-(3, 'Receita Ordinária'),
-(4, 'Receita Extraordinária');
-
-insert into tb_statusmovimentacaofinanceira(IdStatusMovimentacaoFinanceira, StatusMovimentacaoFinanceira)
-values(1,'Em análise'),
-(2,'Autorizada'),
-(3,'Não autorizada'),
-(4,'Registrada'),
-(5,'Finalizada');
-
-
+﻿INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('1', '1', 'Morador');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('2', '1', 'Sindico');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('3', '1', 'Visitante');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('4', '1', 'Administradora');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('5', '1', 'AdministradorSistema');
