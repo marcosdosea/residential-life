08ba38defc18c3ae55e05ab2b6295f1b157dba05
andres.s.menendez@gmail.com <andres.s.menendez@gmail.com@7838a4c6-20ae-e5f0-4d70-4ef4a1f1d643>
2014-08-09 12:43:24 +0000

    Alteração na inicialização dos perfis

--- trunk/Codigo/inicializaBD.sql
+++ trunk/Codigo/inicializaBD.sql
@@ -2,4 +2,8 @@
 INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('2', '1', 'Sindico');
 INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('3', '1', 'Visitante');
 INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('4', '1', 'Administradora');
-INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('5', '1', 'AdministradorSistema');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('5', '1', 'Proprietario');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('6', '1', 'Funcionario');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('7', '1', 'Responsavel');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('8', '1', 'Profissional');
+INSERT INTO `residentialbd`.`my_aspnet_roles` (`id`, `applicationId`, `name`) VALUES ('9', '1', 'AdministradorSistema');
